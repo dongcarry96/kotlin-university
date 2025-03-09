79f6314d9c3e836c71396ef802326e0acb87ee9a
dong <fhakwpak6@gmail.com>
2025-03-09 12:01:07 +0900

    chore : 기본 설정

---
+++ src/test/kotlin/com/dong/university/UniversityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dong.university
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
